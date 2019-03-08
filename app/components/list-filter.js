f7fe6ce7b67970c599e0b39aef96dfa2cc44635c
Pravin Lokhande <pravin.lokhande@cuelogic.com>
2019-03-08 14:26:56 +0530

    asynchronous requests handled.

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -9,13 +9,19 @@ export default Component.extend({
 	value: '',
 	init(){
 		this._super(...arguments);
-		this.filter('').then((results) => this.set('results',results));
+		this.filter('').then((allResults) => {
+      this.set('results', allResults.results);
+    });
 	},
 	actions: {
 		handleFilterEntry(){
 			let filterInputValue = this.value;
 			let filterAction = this.filter;
-			filterAction(filterInputValue).then((filterResults)=> this.set('results',filterResults));
+			filterAction(filterInputValue).then((filterResults) => {
+        if (filterResults.query === this.value) {
+          this.set('results', filterResults.results);
+        }
+      });
 		}
 	}
 });
